a3be3c9b09c09b2863bb416d5be732e334ad6b8c
Harley Inbody <hinbody@gmail.com>
2016-01-18 11:32:01 -0500

    Adds function to get all data from all open tickets

--- get_tickets.py
+++ get_tickets.py
@@ -6,3 +6,14 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+baseurl = config['FreshDesk']['base_url']
+api_key = config['FreshDesk']['api_key']
+b64string = base64.b64encode(api_key.encode()).decode('ascii')
+auth = "Basic %s" % b64string
+headers = {"Authorization": auth}
+
+def get_all_open_tickets():
+  """Returns a Requests object containing all data from all open tickets"""
+  r = requests.get(baseurl + '/helpdesk/tickets/filter/open?format=json', headers = headers)
+  return r
