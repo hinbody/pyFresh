93ceef4d3884692951b0be33ed8024b30c4f8063
Harley Inbody <hinbody@gmail.com>
2016-02-02 14:09:10 -0500

    checks to see if a ticket id is already stored in the db, and only add it if it isnt

--- get_tickets.py
+++ get_tickets.py
@@ -3,6 +3,7 @@ import json
 import requests
 import base64
 import configparser
+import sqlite3
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -25,6 +26,54 @@ def get_all_open_tickets():
 
 tickets, tickets1 = get_all_open_tickets()
 
+db_connect = sqlite3.connect('freshdata.db')
+cursor = db_connect.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS tickets
+                  (created_at text,
+                  agent text,
+                  ticket_number text,
+                  subject text,
+                  due_by text,
+                  requester text,
+                  priority text)''')
+
+def add_tickets_to_db():
+  for ticket in tickets:
+    ticket_number = ticket['id']
+    ticket_id = cursor.execute('''SELECT * FROM tickets WHERE
+        ticket_number=?''', (ticket_number,))
+    if ticket_id.fetchall() != []:
+      print('already there')
+    else:
+      data = []
+      data.append(ticket['created_at'])
+      data.append(ticket['responder_id'])
+      data.append(ticket['id'])
+      data.append(ticket['subject'])
+      data.append(ticket['due_by'])
+      data.append(ticket['requester_name'])
+      data.append(ticket['priority'])
+      cursor.execute('''INSERT INTO tickets (created_at, agent, ticket_number,
+      subject, due_by, requester, priority) VALUES (?, ?, ?, ?, ?, ?, ?)''', data)
+  for ticket in tickets1:
+    ticket_id = cursor.execute('''SELECT * FROM tickets WHERE ticket_number=?''',
+        (ticket['id'],))
+    if ticket_id.fetchall() != []:
+      print('already there')
+    else:
+      data = []
+      data.append(ticket['created_at'])
+      data.append(ticket['responder_id'])
+      data.append(ticket['id'])
+      data.append(ticket['subject'])
+      data.append(ticket['due_by'])
+      data.append(ticket['requester_name'])
+      data.append(ticket['priority'])
+      cursor.execute('''INSERT INTO tickets (created_at, agent, ticket_number,
+      subject, due_by, requester, priority) VALUES (?, ?, ?, ?, ?, ?, ?)''', data)
+    #print(ticket['created_at'],ticket['responder_id'],ticket['id'],ticket['subject'],ticket['due_by'],ticket['requester_name'],ticket['priority'])
+add_tickets_to_db()
+db_connect.commit()
 #for stuff in tickets[1]:
 #  print(stuff)
 #for ticket in tickets:
